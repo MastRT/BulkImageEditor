ee3a1d5c07c7479652ba97deb282cc83a7246e18
Reza <reza.rr478@gmail.com>
2025-08-09 13:22:25 -0700

    fixed bug

--- main_window.py
+++ main_window.py
@@ -1,18 +1,25 @@
-from PyQt5.QtWidgets import QLabel,QMainWindow,QLineEdit,QPushButton,QFileDialog
-from PIL import Image,ImageDraw,ImageFont
+from PyQt5.QtWidgets import QLabel,QMainWindow,QLineEdit,QPushButton,QFileDialog,QListWidget,QMessageBox
+from PyQt5.QtGui import QPixmap
+from PIL import Image,ImageDraw,ImageFont,ImageQt
 from pathlib import Path
-from tkinter import filedialog
+from PIL.ImageQt import ImageQt
 
 class Form(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setFixedSize(500,500)
+        self.setFixedSize(700,700)
 
         #label
         inputlbl = QLabel(self)
         inputlbl.setText("text to add to picture:")
         inputlbl.move(50,50)
+
+        #MessageBox
+        self.message = QMessageBox(self)
+        self.message.setText("that's it")
+        self.message.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        
 
         #inputs
         self.textToAddtxtbox = QLineEdit(self)
@@ -34,6 +41,28 @@ class Form(QMainWindow):
         addbtn.move(400,100)
         addbtn.clicked.connect(self.edit_files)
 
+        #Preview the image
+        self.previewlbl = QLabel(self)
+        self.previewlbl.move(300,200)
+        self.previewlbl.setFixedSize(300,300)
+    
+
+        #list of selected files
+        self.listOfSelectedFiles = QListWidget(self)
+        self.listOfSelectedFiles.setFixedSize(200,100)
+        self.listOfSelectedFiles.move(50,200)
+        self.listOfSelectedFiles.itemClicked.connect(self.on_item_clicked)
+    
+    def on_item_clicked(self,item):
+        image = Image.open(item.text())
+        imageCopy = image.copy()
+        max_size = (256,256)
+        image.thumbnail(max_size,Image.Resampling.LANCZOS)
+        pixmap = QPixmap(image)
+        self.previewlbl.setPixmap(pixmap)
+        
+
+
     
     def open_file_dialog(self):
         self.listofFiles = []
@@ -46,12 +75,13 @@ class Form(QMainWindow):
         if filenames:
             self.listofFiles.extend([str(Path(filename))
                                      for filename in filenames])
+        self.listOfSelectedFiles.addItems(self.listofFiles)
             
     def edit_files(self):
         for x in self.listofFiles:
             img = Image.open(x)
             draw = ImageDraw.Draw(img)
-            draw.text(((img.width/2),(img.height/2)),self.textToAddtxtbox.text(),font=ImageFont.truetype("arial.ttf",60),fill=(255,0,0),align="center")
+            draw.text((300,69),self.textToAddtxtbox.text(),font=ImageFont.truetype("arial.ttf",60),fill=(255,0,0),align="center")
             print("not yet")
             img.save(f"C:\\Users\\Reza\\Desktop\\gs.png")
             print("saved")
