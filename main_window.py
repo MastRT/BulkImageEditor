76cd0bf30291e62675cb6b5a826e3a7a43854ab4
Reza <reza.rr478@gmail.com>
2025-08-10 13:12:04 -0700

    add preview image
    makes thumbnail and lets you to preview it by clicking the name in list

--- main_window.py
+++ main_window.py
@@ -4,6 +4,8 @@ from PIL import Image,ImageDraw,ImageFont,ImageQt
 from pathlib import Path
 import os
 
+counter = 50
+
 class Form(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -11,6 +13,7 @@ class Form(QMainWindow):
         self.setFixedSize(700,700)
 
         self.fileNameAndAddress = []
+        self.textbox = []
 
         #label
         inputlbl = QLabel(self)
@@ -43,10 +46,19 @@ class Form(QMainWindow):
         addbtn.move(400,100)
         addbtn.clicked.connect(self.edit_files)
 
+        #Add more textbox
+        addMoreTextBoxbtn = QPushButton(self)
+        addMoreTextBoxbtn.setText("+")
+        addMoreTextBoxbtn.move(400,150)
+        addMoreTextBoxbtn.clicked.connect(self.addMoretxtBoxes)
+
         #Preview the image
         self.previewlbl = QLabel(self)
+        self.previewlbl.setText("Empty")
         self.previewlbl.move(300,200)
         self.previewlbl.setFixedSize(300,300)
+        self.previewlbl.setStyleSheet("border: 2px solid black;" \
+        "text-align: justify")
     
 
         #list of selected files
@@ -54,18 +66,31 @@ class Form(QMainWindow):
         self.listOfSelectedFiles.setFixedSize(200,100)
         self.listOfSelectedFiles.move(50,200)
         self.listOfSelectedFiles.itemClicked.connect(self.on_item_clicked)
+
+
+
+    def addMoretxtBoxes(self):
+        box = QLineEdit(self)
+        yPosition = counter+50
+        box.move(200,yPosition)
+        box.show()
+        
+
+        
     
     def on_item_clicked(self,item):
-        for address in self.fileNameAndAddress:
-            if item.text() == address[0]:
-                print(address[0])
-
-        # image = Image.open(item.text())
-        # max_size = (256,256)
-        # image.thumbnail(max_size,Image.Resampling.LANCZOS)
-        # image.save("C:\\Users\\Reza\\Desktop\\thumb.jpg")
-        # pixmap = QPixmap(item.text())
-        # self.previewlbl.setPixmap(pixmap)
+        for address in self.fileNameAndAddress: # looking to find the location of corespondong selected image
+            # if item.text() != "":
+                if item.text() == address[0]:   #check if the file names are Same
+                    try: #Try opening the thumbnail if it was created before
+                        pixmap = QPixmap(f"C:\\Users\\Reza\\Desktop\\temp\\{item.text()}")
+                        self.previewlbl.setPixmap(pixmap)
+                    finally: #if the thumbnail was not created,tries to create it
+                        image = Image.open(address[1])
+                        copy = image.copy()
+                        max_size = (256,256)
+                        image.thumbnail(max_size,Image.Resampling.LANCZOS)
+                        image.save(f"C:\\Users\\Reza\\Desktop\\temp\\{item.text()}")
         
 
     
@@ -88,12 +113,10 @@ class Form(QMainWindow):
             
 
     def edit_files(self):
-        for x in self.listofFiles:
-            img = Image.open(x)
+        for x in self.fileNameAndAddress:
+            img = Image.open(x[1])
             draw = ImageDraw.Draw(img)
             draw.text((300,69),self.textToAddtxtbox.text(),font=ImageFont.truetype("arial.ttf",60),fill=(255,0,0),align="center")
-            print("not yet")
-            img.save(f"C:\\Users\\Reza\\Desktop\\gs.png")
-            print("saved")
+            img.save(f"C:\\Users\\Reza\\Desktop\\result\\{x[0]}")
 
             
